81f9e707c39ecbc62649fc64c6dd79f3521d5e6e
GusBedasi <ggustavobedasi@gmail.com>
2020-06-28 21:23:12 -0300

    Pyhton-Diagonal-Difference

---
+++ Diagonal_Difference.py
@@ -0,0 +1,25 @@
+def diagonalDifference(arr):
+    # Write your code here
+    
+    rightToLeft = []
+    leftToRight = []
+
+    for i in range(len(arr)):
+        leftToRight.append(arr[i][i])
+        #print(arr[i][i])
+
+    counter = len(arr) -1 # === 2 
+    print(counter, len(arr))
+    for i in range(len(arr)):
+        rightToLeft.append(arr[i][counter])
+        counter -= 1
+
+    result = abs(sum(rightToLeft) - sum(leftToRight))
+
+    #print(leftToRight, " = ", sum(leftToRight))
+    #print(rightToLeft, " = ", sum(rightToLeft))
+    #print(sum(leftToRight), " - ", sum(rightToLeft), " = ", result)
+    #print("")
+
+diagonalDifference([[1, 2, 3], [4, 5, 6], [9, 8, 9]])
+diagonalDifference([[11,2,4],[4,5,6],[10,8,-12]])
